a638d72ae22a75f708428b7d4ae0b206c9586294
nikita <nikita.goutor@gmail.com>
2024-11-13 21:33:22 +0300

    Init.

---
+++ src/main/java/com/example/application/data/entity/Company.java
@@ -0,0 +1,27 @@
+package com.example.application.data.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Formula;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Company extends AbstractEntity {
+    @NotBlank
+    private String name;
+
+    @OneToMany(mappedBy = "company")
+    @Nullable
+    private List<Contact> employees = new LinkedList<>();
+
+    @Formula("(select count(c.id) from Contact c where c.company_id = id)")
+    private int employeeCount;
+}
---
+++ src/main/java/com/example/application/data/entity/Contact.java
@@ -0,0 +1,39 @@
+package com.example.application.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Contact extends AbstractEntity {
+
+    @NotEmpty
+    private String firstName = "";
+
+    @NotEmpty
+    private String lastName = "";
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    @NotNull
+    @JsonIgnoreProperties({"employees"})
+    private Company company;
+
+    @NotNull
+    @ManyToOne
+    private Status status;
+
+    @Email
+    @NotEmpty
+    private String email = "";
+}
